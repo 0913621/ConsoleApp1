9afa0109d856fdb9464d83963f8636166913f362
0913621 <0913621@hr.nl>
2020-02-18 13:53:10 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = "";
+        for (int i = 5; i <= 10; i += 2)
+            s += i + " ";
+
+        for (int i = 10; i >= 3; i--)
+            if (i % 2 == 0)
+                s += i + " ";
+        Console.WriteLine(s);
+    }
+}
+
